262a09b1795c2b132089d56c17562a55dd51086e
Milan V. Kecojević <staog@gmx.com>
2018-12-30 18:49:01 +0100

    Create initial-set-up.sh

---
+++ linux-mint-initial-set-up/initial-set-up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Remove Mono and Orca
+sudo apt remove -y mono-runtime-common gnome-orca
+echo "Mono and Orca are gone!"
+
+# Install Audacious and VLC
+apt install -y audacious vlc
+echo "Audacious and VLC have been installed." && sleep 10
