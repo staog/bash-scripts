10e6a055f1dca31e9b255694751563d6ae0d5d12
Milan V. Kecojević <staog@gmx.com>
2019-04-28 15:46:26 +0200

    Update initial-set-up.sh

--- linux-mint-initial-set-up/initial-set-up.sh
+++ linux-mint-initial-set-up/initial-set-up.sh
@@ -5,5 +5,5 @@ sudo apt remove -y mono-runtime-common gnome-orca
 echo "Mono and Orca are gone!"
 
 # Install Audacious and VLC
-apt install -y audacious vlc
-echo "Audacious and VLC have been installed." && sleep 5
+apt install -y nodejs npm audacious vlc
+echo "NodeJS, npm, Audacious and VLC have been installed." && sleep 5
